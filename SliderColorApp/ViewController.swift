d2acf52ed62aeb8ea23e76987394ca96fe967144
Viktor Smirnov <smirn04@yandex.ru>
2023-09-29 14:54:32 +0300

    Initial Commit

---
+++ SliderColorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderColorApp
+//
+//  Created by Viktor Smirnov on 29.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

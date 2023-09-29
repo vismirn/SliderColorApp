403f5d471b9bcdb2ca525c56e3e73ba854b755ba
Viktor Smirnov <smirn04@yandex.ru>
2023-09-29 15:30:24 +0300

    Создал Outlets, (colorView, labels, countLabels, sliders), Actions (sliders)

--- SliderColorApp/ViewController.swift
+++ SliderColorApp/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var countRedLabel: UILabel!
+    @IBOutlet weak var countGreenLabel: UILabel!
+    @IBOutlet weak var countBlueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func redSliderAction() {
+    }
+    @IBAction func greenSliderAction() {
+    }
+    @IBAction func blueSliderAction() {
+    }
+    
 
 }
 
